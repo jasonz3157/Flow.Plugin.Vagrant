924f5c323acfea7c2d22cd8fcec85b6cbd962f0e
jasonz3157 <jasonz3157@gmail.com>
2024-03-06 18:12:56 +0800

    test hide flow

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ sys.path.append(os.path.join(parent_folder_path, "lib"))
 sys.path.append(os.path.join(parent_folder_path, "plugin"))
 
 
-from flowlauncher import FlowLauncher
+from flowlauncher import FlowLauncher, FlowLauncherAPI
 
 
 class Vagrant(FlowLauncher):
@@ -79,7 +79,8 @@ class Vagrant(FlowLauncher):
             return
 
     def control_vm(self, id, action):
-        subprocess.Popen(["vagrant", action, id], shell=True).decode()
+        subprocess.Popen(["vagrant", action, id], shell=True)
+        FlowLauncherAPI.hide_app()
 
 
 if __name__ == "__main__":
