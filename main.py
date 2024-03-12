76e39e688f8c5e840b98a08cc86926ab8174b386
jasonz3157 <jasonz3157@gmail.com>
2024-03-12 16:33:29 +0800

    SubTitle

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ class Vagrant(FlowLauncher):
                 msgs.append(
                     {
                         "Title": f"{action.upper()} {vm.name.strip()}",
-                        # "SubTitle": f"{vm.id.strip()}",
+                        "SubTitle": f"{vm.id.strip()}",
                         "IcoPath": f"Images/{action}.png",
                         "jsonRPCAction": {
                             "method": "control_vm",
