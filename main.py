f8213f312a2e5c937dffd53a314f28fc55fc0702
jasonz3157 <jasonz3157@gmail.com>
2024-07-17 15:02:49 +0800

    021

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ from flowlauncher import FlowLauncher
 class Vagrant(FlowLauncher):
     # running saved poweroff
     # up suspend halt
+    vms = []
+
     def list_vm(self, prune=False):
         cmd = "vagrant global-status --prune" if prune else "vagrant global-status"
         output = subprocess.check_output(cmd, shell=True).decode("utf-8")
@@ -32,30 +34,53 @@ class Vagrant(FlowLauncher):
                 continue
             else:
                 all_vms.append(m.groupdict())
-        return all_vms
+        self.vms = all_vms
+
+    def control_vm(self, vm_id, action):
+        subprocess.run(["vagrant", action, vm_id])
 
     def query(self, arguments: str):
+        self.list_vm()
         # 没有参数时，获取当前 vm 列表
         if not arguments:
-            rlist = [
+            return [
                 {
                     "Title": _.get("name"),
-                    "SubTitle": _.get("state") + "," + _.get("id"),
+                    "SubTitle": _.get("state") + ", " + _.get("id"),
                     "IcoPath": (
                         "Images/ok.png"
                         if _.get("state") == "running"
                         else "Images/notok.png"
                     ),
                 }
-                for _ in self.list_vm()
+                for _ in self.vms
             ] + [
                 {
                     "Title": "刷新",
-                    "SubTitle": "重新获取虚拟机列表",
+                    "SubTitle": "重新获取虚拟机列表及状态",
                     "IcoPath": "Images/reload.png",
                     "jsonRPCAction": {"method": "list_vm", "parameters": [True]},
                 },
             ]
+        if arguments in [_.get("name") for _ in self.vms]:
+            vm = [_ for _ in self.vms if _.get("name") == arguments][0]
+            if vm.get("state") == "running":
+                actions = ["suspend", "halt"]
+            else:
+                actions = ["up"]
+            return [
+                {
+                    "Title": f"{action.upper()} {arguments}",
+                    "IcoPath": f"Images/{action}.png",
+                    "jsonRPCAction": {
+                        "method": "control_vm",
+                        "parameters": [vm.get("id"), action],
+                    },
+                }
+                for action in actions
+            ]
+        else:
+            return []
 
 
 if __name__ == "__main__":
